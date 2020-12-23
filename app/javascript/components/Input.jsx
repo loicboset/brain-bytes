71401a2b75fe9a3516a4ec5fce59333f47f82521
Loïc Boset <loic.boset@gmail.com>
2020-12-23 08:25:59 +0100

    making input field

--- app/javascript/components/Input.jsx
+++ app/javascript/components/Input.jsx
@@ -4,11 +4,19 @@ const Input = () => {
 
   return (
     <div className='w-75 mx-auto mt-2'>
-      <textarea 
+      {/* <span 
+        contentEditable
+        role="textbox"
         className='w-100 border-bottom'
-        placeholder='Insert brain bytes here'
-        type="text" id="bytes" name="bytes" required minlength="4" maxlength="140" size="10" 
-      />
+        placeholder='Share a brain byte'
+        id='byte'
+      >
+      </span> */}
+      <span className='absolute text-muted pointer-events-none'>Share a brain byte</span>
+      <div 
+        className='border-bottom outline-none' 
+        contentEditable data-text="Enter text here"
+      ></div>
     </div>
   )
 };
