8591da397ee810f60f3c5c9f86dcc806eb4e227f
loicboset <loic.boset@gmail.com>
2020-12-29 09:47:24 +0100

    adding limit to votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,11 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :byte
+
+  validates :count, presence: true
+  validates :user_id, presence: true
+  validates :byte_id, presence: true
+
+  validates :user_id, uniqueness: { scope: :byte_id,
+    message: "a user can only give one vote per byte" }
 end
